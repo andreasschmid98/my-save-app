cf6fde2e0b6314329fd71664cdf79b89ef814494
Andreas Schmid <schmidandreas.ul@gmail.com>
2023-11-09 23:35:23 +0100

    Add IgnorePointer to all actions

--- lib/screens/dashboard/entry/delete_single_entry.dart
+++ lib/screens/dashboard/entry/delete_single_entry.dart
@@ -6,47 +6,52 @@ import '../../../models/entry.dart';
 import '../../../providers/project_provider.dart';
 
 class DeleteSingleEntry extends StatelessWidget {
-  const DeleteSingleEntry({
+  DeleteSingleEntry({
     super.key,
     required this.entry,
   });
 
   final Entry entry;
+  bool _ignoreInput = false;
 
   @override
   Widget build(BuildContext context) {
-    return Wrap(children: [
-      Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Column(
-          children: [
-            Text(AppLocalizations.of(context).deleteEntry),
-            const SizedBox(
-              height: 15,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FilledButton(
-                    onPressed: () async {
-                      await context
-                          .read<ProjectProvider>()
-                          .deleteEntryById(entry.id)
-                          .then((response) => _onSuccess(context))
-                          .onError((error, stackTrace) => _onError(context));
-                    },
-                    child: Text(AppLocalizations.of(context).yes)),
-                FilledButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text(AppLocalizations.of(context).no))
-              ],
-            )
-          ],
+    return IgnorePointer(
+      ignoring: _ignoreInput,
+      child: Wrap(children: [
+        Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              Text(AppLocalizations.of(context).deleteEntry),
+              const SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FilledButton(
+                      onPressed: () async {
+                        _ignoreInput = true;
+                        await context
+                            .read<ProjectProvider>()
+                            .deleteEntryById(entry.id)
+                            .then((response) => _onSuccess(context))
+                            .onError((error, stackTrace) => _onError(context));
+                      },
+                      child: Text(AppLocalizations.of(context).yes)),
+                  FilledButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text(AppLocalizations.of(context).no))
+                ],
+              )
+            ],
+          ),
         ),
-      ),
-    ]);
+      ]),
+    );
   }
 
   void _onError(BuildContext context) {
--- lib/screens/home/project/delete_project.dart
+++ lib/screens/home/project/delete_project.dart
@@ -6,47 +6,53 @@ import '../../../models/project.dart';
 import '../../../providers/project_provider.dart';
 
 class DeleteProject extends StatelessWidget {
-  const DeleteProject({
+  DeleteProject({
     super.key,
     required this.project,
   });
 
   final Project project;
 
+  bool _ignoreInput = false;
+
   @override
   Widget build(BuildContext context) {
-    return Wrap(children: [
-      Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Column(
-          children: [
-            Text(AppLocalizations.of(context).deleteProject),
-            const SizedBox(
-              height: 15,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FilledButton(
-                    onPressed: () async {
-                      await context
-                          .read<ProjectProvider>()
-                          .deleteProjectById(project.id)
-                          .then((response) => _onSuccess(context))
-                          .onError((error, stackTrace) => _onError(context));
-                    },
-                    child: Text(AppLocalizations.of(context).yes)),
-                FilledButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text(AppLocalizations.of(context).no))
-              ],
-            )
-          ],
+    return IgnorePointer(
+      ignoring: _ignoreInput,
+      child: Wrap(children: [
+        Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              Text(AppLocalizations.of(context).deleteProject),
+              const SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FilledButton(
+                      onPressed: () async {
+                        _ignoreInput = true;
+                        await context
+                            .read<ProjectProvider>()
+                            .deleteProjectById(project.id)
+                            .then((response) => _onSuccess(context))
+                            .onError((error, stackTrace) => _onError(context));
+                      },
+                      child: Text(AppLocalizations.of(context).yes)),
+                  FilledButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text(AppLocalizations.of(context).no))
+                ],
+              )
+            ],
+          ),
         ),
-      ),
-    ]);
+      ]),
+    );
   }
 
   void _onError(BuildContext context) {
